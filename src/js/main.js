b318da5b78e0bce5b00b09dc15a3a446198e9a04
heyhurricane <dinohka@mail.ru>
2020-07-29 23:42:59 +0400

    add js to section coming tours

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var windowHeight = $(window).height();
     if ($(this).scrollTop() > 0) {
-      // $('.scroll-up').fadeIn();
+      $('.scroll-up').fadeIn();
       if (document.getElementById("header") !== null) {
         document.getElementById("header").style.background = "#5a52a5";
       }
@@ -29,6 +29,76 @@ $(document).ready(function () {
       nextEl: '.tourswiper-button-next',
       prevEl: '.tourswiper-button-prev',
     },
+    fadeEffect: {
+      crossFade: true,
+    },
+    effect: "fade",
+    
   });
-  
+
+  // переключение вкладок слайдера
+  $('.comingtours__tour').click(function () {
+     var page = $(this).attr('id');
+     $('.comingtours__tour').not(this).removeClass('tour--active');
+     $(this).addClass('tour--active');
+     if (page == '1') {
+        mySwiper.slideTo(page);
+     }
+     if (page == '2') {
+        mySwiper.slideTo(page);
+     }
+     if (page == '3') {
+        mySwiper.slideTo(page);
+     }
+  });
+
+  // переключение по кнопкам
+  $('.tourswiper-button-prev').on('click', function () {
+    const index = mySwiper.realIndex;
+    var stepBtn = $('.comingtours__tour').slice(index, index + 1);
+    var page = stepBtn.attr('id');
+    console.log(page);
+    $('.comingtours__tour').not(stepBtn).removeClass('tour--active');
+    stepBtn.addClass('tour--active');
+ 
+
+  });
+
+  $('.tourswiper-button-next').on('click', function () {
+    // stepBtn.toggleClass('step--active');
+    const index = mySwiper.realIndex;
+    var stepBtn = $('.comingtours__tour').slice(index, index + 1);
+    var page = stepBtn.attr('id');
+    console.log(page);
+    $('.comingtours__tour').not(stepBtn).removeClass('tour--active');
+    stepBtn.addClass('tour--active');
+    // stepBtn.toggleClass('step--active');
+
+  });
+
+ // переключение слайдера по нажатию кнопки Тура в header
+  $('.menu__tour-season').click(function () {
+    var page = $(this).attr('id');
+    console.log(page);
+    var stepBtn;
+    if (page == 'fall') {
+      const id=1;
+      stepBtn = $('.comingtours__tour').slice(id-1, id);
+      mySwiper.slideTo(1);
+    }
+    if (page == 'winter') {
+      const id = 2;
+      stepBtn = $('.comingtours__tour').slice(id-1, id);
+      mySwiper.slideTo(2);
+    }
+    if (page == 'summer') {
+      const id = 3;
+      stepBtn = $('.comingtours__tour').slice(id-1, id);
+      mySwiper.slideTo(3);
+    }
+    $('.comingtours__tour').not(stepBtn).removeClass('tour--active');
+    stepBtn.addClass('tour--active');
+  });
+
+ 
 });
